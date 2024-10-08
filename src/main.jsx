1e362f262103e1c5f944a9244159a21d80676450
sanggusseu <sanggusseu@gmail.com>
2024-10-08 15:50:02 +0900

    react-router 설치 및 설정

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,37 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import MyWish from './pages/MyWish.jsx';
+import OtherWishes from './pages/OtherWishes.jsx';
+
+const isLoggedIn = false;
+const getMainElement = isLoggedIn =>
+  isLoggedIn ? <MyWish /> : <OtherWishes />;
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '',
+        element: getMainElement(isLoggedIn),
+      },
+      {
+        path: 'other',
+        element: <OtherWishes />,
+      },
+      {
+        path: 'my',
+        element: <MyWish />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
