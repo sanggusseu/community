0ea7292ec5ca56b8f6bb77c4e465af4dece4729b
sanggusseu <sanggusseu@gmail.com>
2024-10-08 19:45:40 +0900

    Button 컴포넌트 type, style 변경

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
-export default function Button({ children, handleOnClick }) {
+export default function Button({ children, handleOnClick, type, addStyle }) {
   return (
     <button
-      type="button"
-      className="flex items-center justify-center bg-primary min-h-10 rounded-md px-3 py-2 text-light hover:bg-opacity-50 transition-all duration-200"
+      type={type || 'button'}
+      className={`flex items-center justify-center bg-primary min-h-10 rounded-md px-3 py-2 text-light hover:bg-opacity-50 transition-all duration-200 ${addStyle}`}
       onClick={handleOnClick}
     >
       {children}
