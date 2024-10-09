8ef2298e7c7d511b1e0ad4bc947564d6b5d085bf
sanggusseu <sanggusseu@gmail.com>
2024-10-09 18:07:22 +0900

    로그인 상태 감지 후 경로 설정

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react';
+import { StrictMode, useContext } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
@@ -6,41 +6,48 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import MyWish from './pages/MyWish.jsx';
 import OtherWishes from './pages/OtherWishes.jsx';
 import MyPage from './pages/MyPage.jsx';
-import { AuthProvider } from './context/AuthContext.jsx';
+import { AuthContext, AuthProvider } from './context/AuthContext.jsx';
 
-const isLoggedIn = false;
-const getMainElement = isLoggedIn =>
-  isLoggedIn ? <MyWish /> : <OtherWishes />;
+const getMainElement = isLoggedIn => {
+  return isLoggedIn ? <MyWish /> : <OtherWishes />;
+};
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '',
-        element: getMainElement(isLoggedIn),
-      },
-      {
-        path: 'other',
-        element: <OtherWishes />,
-      },
-      {
-        path: 'my',
-        element: <MyWish />,
-      },
-      {
-        path: 'mypage',
-        element: <MyPage />,
-      },
-    ],
-  },
-]);
+const AppRoutes = () => {
+  const { user } = useContext(AuthContext);
+  const isLoggedIn = !!user;
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        {
+          path: '',
+          element: getMainElement(isLoggedIn),
+        },
+        {
+          path: 'other',
+          element: <OtherWishes />,
+        },
+        {
+          path: 'my',
+          element: <MyWish />,
+        },
+        {
+          path: 'mypage',
+          element: <MyPage />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+};
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-    <RouterProvider router={router} />
+      <AppRoutes />
     </AuthProvider>
   </StrictMode>,
 );
