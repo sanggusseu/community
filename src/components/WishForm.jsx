e9ee42bc4596c19cfef7d58ca89583b691f605dc
sanggusseu <sanggusseu@gmail.com>
2024-10-12 15:58:03 +0900

    WishForm 컴포넌트 생성

---
+++ src/components/WishForm.jsx
@@ -0,0 +1,25 @@
+import InputField from './form/InputField';
+import TextArea from './form/TextArea';
+import Form from './form/Form';
+
+const WishForm = ({ handleOnSubmit, initialValue = {}, submitBtn }) => {
+  return (
+    <div className=" dark:text-white text-gray-900">
+      <div className="flex items-center my-8">
+        <h1 className="text-3xl font-bold">소원 작성하기</h1>
+      </div>
+      <Form
+        OnSubmit={handleOnSubmit}
+        initialValue={initialValue}
+        submitBtn={submitBtn}
+      >
+        <InputField id="title" label="제목" name="title" />
+        <InputField id="link" label="구매처" type="url" name="link" />
+        <InputField id="price" label="가격" type="number" name="price" />
+        <TextArea id="memo" label="메모" name="memo" />
+      </Form>
+    </div>
+  );
+};
+
+export default WishForm;
