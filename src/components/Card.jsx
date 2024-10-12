aa8102ea61d2be7a102437f2ee8ef32dc802c257
sanggusseu <sanggusseu@gmail.com>
2024-10-12 15:46:38 +0900

    Card 컴포넌트 속성 및 요소 수정

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { TbMoneybag, TbBuildingStore, TbUser } from 'react-icons/tb';
 import { CiCalendarDate } from 'react-icons/ci';
+import { Link } from 'react-router-dom';
 
-export default function Card({ title, link, memo, author, createdAt, price }) {
+export default function Card({
+  title,
+  link,
+  memo,
+  author,
+  createdAt,
+  price,
+  id,
+}) {
   return (
-    <div className="rounded overflow-hidden shadow-lg bg-white p-6">
-      <strong className="font-bold text-xl mb-2 truncate">{title}</strong>
+    <Link
+      to={`/posts/${id}`}
+      className="rounded overflow-hidden shadow-lg bg-white p-6"
+    >
+      <strong className="font-bold text-xl mb-2 block truncate">{title}</strong>
       <p className="text-gray-700 text-base mb-4 truncate">{memo}</p>
       <div className="flex items-center text-gray-600 mb-2">
         <TbMoneybag className="mr-2" />
@@ -13,7 +25,12 @@ export default function Card({ title, link, memo, author, createdAt, price }) {
       </div>
       <div className="flex items-center text-gray-600 mb-2">
         <TbBuildingStore className="mr-2" />
-        <a href={link} target="_blank" className="hover:underline">
+        <a
+          href={link}
+          target="_blank"
+          className="hover:underline"
+          onClick={e => e.stopPropagation()}
+        >
           구매처
         </a>
       </div>
@@ -25,6 +42,6 @@ export default function Card({ title, link, memo, author, createdAt, price }) {
         <CiCalendarDate className="mr-2" />
         <span>{createdAt}</span>
       </div>
-    </div>
+    </Link>
   );
 }
