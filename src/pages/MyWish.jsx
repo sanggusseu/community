bcaeb72e69553cf26b381985da52963211c9f0ea
sanggusseu <sanggusseu@gmail.com>
2024-10-13 10:22:31 +0900

    MyWish, OtherWishes 페이지 내용 설정

--- src/pages/MyWish.jsx
+++ src/pages/MyWish.jsx
@@ -1,3 +1,24 @@
+import { useContext, useEffect } from 'react';
+import { WishContext } from '../context/WishContext';
+import { AuthContext } from '../context/AuthContext';
+import WishContainer from '../components/WishContainer';
+import { useNavigate } from 'react-router-dom';
+
 export default function MyWish() {
-  return <div>내 소원</div>;
+  const { posts, fetchFilteredPosts } = useContext(WishContext);
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+  useEffect(() => {
+    const handleGetMyPosts = async (query, value) => {
+      await fetchFilteredPosts(query, value);
+    };
+
+    if (user) {
+      handleGetMyPosts('userId', user.userId);
+    } else {
+      alert('로그인 후 이용 가능합니다!');
+      navigate('/');
+    }
+  }, [user]);
+  return <WishContainer posts={posts} />;
 }
--- src/pages/OtherWishes.jsx
+++ src/pages/OtherWishes.jsx
@@ -1,10 +1,13 @@
-import { useContext, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import Card from '../components/Card';
+import { useContext, useEffect, useState } from 'react';
 import { WishContext } from '../context/WishContext';
+import WishContainer from '../components/WishContainer';
+import SearchInput from '../components/SearchInput';
 
 export default function OtherWishes() {
   const { posts, fetchPosts } = useContext(WishContext);
+  const { fetchFilteredPosts } = useContext(WishContext);
+  const [queryData, setQueryData] = useState({ query: 'title', value: '' });
+
   useEffect(() => {
     const handleGetPosts = async () => {
       await fetchPosts();
@@ -13,26 +16,18 @@ export default function OtherWishes() {
     handleGetPosts();
   }, []);
 
+  const handleSearch = e => {
+    const { name, value } = e.target;
+    setQueryData(prevData => {
+      const newQueryData = { ...prevData, [name]: value };
+      fetchFilteredPosts(newQueryData.query, newQueryData.value);
+      return newQueryData;
+    });
+  };
+
   return (
-    <section>
-      <div className="flex justify-end mb-2">
-        <Link
-          to="/create"
-          className="inline-flex items-center justify-center bg-primary min-h-10 rounded-md px-3 py-2 text-light hover:bg-opacity-50 transition-all duration-200"
-        >
-          소원 작성하기
-        </Link>
-      </div>
-      {posts.length === 0 && (
-        <strong className="text-3xl dark:text-gray-200 text-dark block mb-8">
-          제일 먼저 소원을 작성해 보세요!
-        </strong>
-      )}
-      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {posts.map(post => (
-          <Card key={post.id} {...post} />
-        ))}
-      </div>
-    </section>
+    <WishContainer posts={posts}>
+      <SearchInput queryData={queryData} handleSearch={handleSearch} />
+    </WishContainer>
   );
 }
